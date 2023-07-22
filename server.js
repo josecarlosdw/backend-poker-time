7c8664eae24586136b59b424a48b696354b1715a
José Carlos do Carmo <josecarlosti@gmail.com>
2023-07-22 17:51:49 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static('dist/planning-poker-app'));
+app.use(express.json());
 
 // Lista de participantes na sala
 const participants = [];
@@ -20,6 +21,30 @@ function findDisconnectedParticipant(socketId) {
   return participants.find((participant) => participant.socketId === socketId);
 }
 
+// Rota para criar uma sala
+app.post('/api/rooms', (req, res) => {
+  try {
+    const { roomCode } = req.body;
+
+    // Carrega o conteúdo do arquivo JSON
+    const roomsData = JSON.parse(fs.readFileSync('rooms.json'));
+
+    // Verifica se a sala já existe no arquivo
+    const existingRoom = roomsData.find((room) => room.roomCode === roomCode);
+    if (existingRoom) {
+      return res.status(400).json({ message: 'Sala já existe' });
+    }
+
+    // Adiciona a nova sala ao array no arquivo
+    roomsData.push({ roomCode });
+    fs.writeFileSync('rooms.json', JSON.stringify(roomsData, null, 2));
+
+    return res.status(201).json({ message: 'Sala criada com sucesso', room: { roomCode } });
+  } catch (error) {
+    console.error('Erro ao criar a sala:', error);
+    return res.status(500).json({ message: 'Erro ao criar a sala' });
+  }
+});
 
 io.on('connection', (socket) => {
   
