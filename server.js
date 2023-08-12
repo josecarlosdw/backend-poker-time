38054ca517bd4382b456633a5ccdf49d4e992b2b
José Carlos do Carmo <josecarlosti@gmail.com>
2023-08-12 16:47:46 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server, {
+const io = socketIO(server, {
   cors: {
     credentials: true,
     origin: '*',
